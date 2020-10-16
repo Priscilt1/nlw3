3e8d4b61381f2fa046ed7cded7e8474c5226f830
Priscila Ribeiro <priscilt.ribeiro@hotmail.com>
2020-10-16 15:17:34 -0300

    incluindo o hbs

--- src/server.js
+++ src/server.js
@@ -5,11 +5,22 @@ const path = require ('path')
 // iniciando o express
 const server = express ()
 
-// criando rota
-server.get('/', (request, response) => {
-    // __dirname informa o caminho absoluto do diretório que contém o arquivo atualmente em execução.
-    console.log(path.join(__dirname, 'views'))
-})
+// criando todas as rotas referentes ao public (arquivos estaticos)
+server
+.use(express.static('public'))
+    // criando rota
+
+    //configurar template engine
+    .set('views', path.join (__dirname, "view"))
+    .set('view engine', 'hbs')
+
+    .get('/', (request, response) => {
+        // __dirname informa o caminho absoluto do diretório que contém o arquivo atualmente em execução.
+        return response.render('index')
+    })
+
 
 // ligar o servidor 
 server.listen(5500)
+
+// post - envia informacao para o back e o get manda informacao
